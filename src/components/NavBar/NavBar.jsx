ad9907305f7899d59c810c2d821900f71b455951
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-06-01 14:16:53 -0400

    incluyendo brand y mejora de estilos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,12 +2,14 @@ import './NavBar.css'
 function NavBar() {
   return (
     <nav className="nav-container">
-        <ul className="nav-main">
-            <li><a href="#">Mantenimiento</a></li>
-            <li><a href="#">Protecciones</a></li>
-            <li><a href="#">Repuestos</a></li>
-            <li><a href="#">Contacto</a></li>
-        </ul>
+      <span className='brand'>RoraimaBike</span>
+      <ul className="nav-main">
+        <li className='nav-item'><a href="/#">Home</a></li>
+        <li className='nav-item'><a href="/#">Mantenimiento</a></li>
+        <li className='nav-item'><a href="/#">Protecciones</a></li>
+        <li className='nav-item'><a href="/#">Repuestos</a></li>
+        <li className='nav-item'><a href="/#">Contacto</a></li>
+      </ul>
     </nav>
   )
 }
