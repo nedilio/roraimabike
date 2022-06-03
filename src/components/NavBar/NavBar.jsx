d2829544e36b7d205acd2ee848c7c262f9c89a97
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-06-03 11:55:13 -0400

    hamburger para nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,9 @@ import NavLink from "./NavLink/NavLink";
 function NavBar() {
   const toggleMobileMenu = () => {
     const menu = document.querySelector('.navbar-collapse');
+    const button = document.querySelector('.hamburger-toggle');
     menu.classList.toggle('active');
+    button.classList.toggle('active');
     // console.log('click menu boton');
   };
   return (
@@ -12,17 +14,14 @@ function NavBar() {
       <nav className="navbar navbar-expand-md navbar-light bg-light">
         <div className="container-fluid">
           <img src="./img/logo-color.png" className="logo-nav" alt="" />
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+          <button 
+            className="hamburger-toggle"
             onClick={toggleMobileMenu}
           >
-            <span className="navbar-toggler-icon"></span>
+              <span className="line-1"></span>
+              <span className="line-2"></span>
+              <span className="line-3"></span>
+            {/* <span className="navbar-toggler-icon"></span> */}
           </button>
           <div className="navbar-collapse" id="navbarSupportedContent">
             <ul className="nav-main navbar-nav me-auto mb-2 mb-lg-0">
