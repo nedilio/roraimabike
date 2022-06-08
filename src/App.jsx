d4d10fa2876321242832804aa7d4ff02c292b016
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-06-08 13:18:26 -0400

    test cambiando a Mayusculas App.css para resolver error netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
-import './app.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import './App.css';
 
 function App() {
   return (
